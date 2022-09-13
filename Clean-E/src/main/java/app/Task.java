f23482f6db75d47c06c9f7b6ecbea38ade7a2aa7
slinningsander <slinningsander@gmail.com>
2022-09-13 12:45:09 +0200

    La til user og task klasser

---
+++ Clean-E/src/main/java/app/Task.java
@@ -0,0 +1,55 @@
+package main.java.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task {
+
+    final private List<String> days =Arrays.asList("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
+    
+    private User assignedUser;
+    private String taskName;
+    private int pointsValue;
+    private boolean completed = false;
+    private String dueDay;
+
+    
+    public Task(User assignedUser, String taskName, int pointsValue, String dueDay) {
+        this.assignedUser = assignedUser;
+        this.taskName = taskName;
+        this.pointsValue = pointsValue;
+        if (days.contains(dueDay.toLowerCase())) {
+            this.dueDay = dueDay;
+        }
+        else {
+            throw new IllegalArgumentException("Choose a valid day.");
+        }
+    }
+
+    public void setTrue() {
+        this.completed = true;
+    }
+
+    public User getAssignedUser() {
+        return assignedUser;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public int getPointsValue() {
+        return pointsValue;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    
+
+    
+
+
+
+}
---
+++ Clean-E/src/main/java/app/User.java
@@ -0,0 +1,47 @@
+package main.java.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    
+    private int points;
+
+    private List<Task> tasks = new ArrayList<>();
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void addPoints(int additionalPoints) {
+        this.points += additionalPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void addTask(Task task) {
+        getTasks().add(task);
+    }
+
+
+
+
+
+    
+}
