6f72f2a611e23697efe229c2d96f4ca8f54cfbd4
slinningsander <slinningsander@gmail.com>
2022-09-16 15:40:29 +0200

    Laget LeaderboardTest, la til getter i Leaderboard

---
+++ gr2244/src/test/java/cleanE/LeaderboardTest.java
@@ -0,0 +1,23 @@
+package cleanE;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cleane.Leaderboard;
+
+public class LeaderboardTest {
+
+    Leaderboard l;
+    
+    @BeforeEach
+    public void setup(){
+        l = new Leaderboard();
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals(0, l.getUsers().size());
+    }
+}
