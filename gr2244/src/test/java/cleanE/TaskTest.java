541e1bed0b173533c2e847e2661087a20e4cc051
slinningsander <slinningsander@gmail.com>
2022-09-16 15:03:42 +0200

    Mer eller mindre ferdig med TaskTest

---
+++ gr2244/src/test/java/cleanE/TaskTest.java
@@ -0,0 +1,40 @@
+package cleanE;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cleane.Task;
+import cleane.User;
+
+public class TaskTest {
+
+    User u;
+    Task t;
+
+    @BeforeEach
+    public void setup(){
+        u = new User("name");
+        t = new Task(u, "taskName", 5, "monday");
+    }
+    
+    @Test
+    public void testConstructor(){
+        assertEquals("taskName", t.getTaskName());
+        assertEquals(u, t.getAssignedUser());
+        assertEquals(5, t.getPointsValue());
+        assertEquals("monday", t.getDueDay());
+        assertEquals(false, t.isCompleted());
+        assertThrows(IllegalArgumentException.class, () -> new Task(u, "taskName", 5, "NotAVaildDay"));
+    }
+
+    @Test
+    public void testSetTrue(){
+        t.setTrue();
+        assertEquals(true, t.isCompleted());
+
+    }
+
+}
