9b8346e68443fb50c65a58df6bc4a1b3654588d1
slinningsander <slinningsander@gmail.com>
2022-09-16 10:42:09 +0200

    La til controller og endret navn på app

---
+++ gr2244/src/main/java/cleane/CleanEApp.java
@@ -0,0 +1,13 @@
+package cleane;
+
+/**
+ * Hello world!
+ *
+ */
+public class CleanEApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
