98928829f4db156866c9a55abc4529e2b62c2c51
unknown <bcfogale@stud.ntnu.no>
2022-09-16 15:45:50 +0200

    test

---
+++ gr2244/src/main/java/cleane/FileManagement.java
@@ -0,0 +1,47 @@
+package cleane;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class FileManagement {
+
+    
+
+    private void writeToFile(Leaderboard leaderboard) {
+
+        
+
+        try {
+            FileOutputStream file = new FileOutputStream("clean-e.txt");
+            ObjectOutputStream output = new ObjectOutputStream(file);
+
+            output.writeObject(leaderboard);
+
+            output.close();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    private Leaderboard readFromFile() {
+        
+
+        try {
+            FileInputStream file = new FileInputStream("clean-e.txt");
+            ObjectInputStream input = new ObjectInputStream(file);
+
+            Leaderboard newLeaderboard = (Leaderboard) input.readObject();
+
+            input.close();
+
+            return newLeaderboard;
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+
+        return null;
+    }
+}
