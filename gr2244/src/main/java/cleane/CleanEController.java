f931ba80229f5a4886f89f691fe77a4d9cf7aa6b
Sandra <sandrml@ntnu.no>
2022-09-19 11:23:45 +0200

    startet på controller

--- gr2244/src/main/java/cleane/CleanEController.java
+++ gr2244/src/main/java/cleane/CleanEController.java
@@ -1,5 +1,26 @@
 package cleane;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
 public class CleanEController {
-    
+
+    private Leaderboard leaderboard;
+    private Task task;
+    private User user;
+
+    @FXML
+    private Button taskButton;
+
+
+    @FXML
+    private ListView<Task> taskView;
+
+    @FXML
+    private void handleNewTask() {
+        Task task = new Task(assignedUser, taskName, pointsValue, dueDay);
+        tasks.addTask(task);
+    }
+
 }
