5654beddd46f59a15b31b20bbe441b5f703a9fed
Sander Garland Slinning <sandergs@stud.ntnu.no>
2022-09-20 11:53:37 +0200

    Fikset app, lagt til jacoco plugin

--- gr2244/src/main/java/cleane/CleanEApp.java
+++ gr2244/src/main/java/cleane/CleanEApp.java
@@ -4,10 +4,26 @@ package cleane;
  * Hello world!
  *
  */
-public class CleanEApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CleanEApp extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("CleanE");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/resources/App.fxml"))));
+        primaryStage.show();
+    }
+
+
+
 }
