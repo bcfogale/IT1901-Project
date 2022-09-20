067c6338b7e59673756589a9bb013586284b900d
Sandra <sandrml@ntnu.no>
2022-09-20 10:01:55 +0200

    Controller: Startet på pop-up for new task

--- gr2244/src/main/java/cleane/CleanEController.java
+++ gr2244/src/main/java/cleane/CleanEController.java
@@ -3,6 +3,8 @@ package cleane;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 
 public class CleanEController {
 
@@ -19,8 +21,30 @@ public class CleanEController {
 
     @FXML
     private void handleNewTask() {
-        Task task = new Task(assignedUser, taskName, pointsValue, dueDay);
-        tasks.addTask(task);
+        //selve dialogpane:
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("New Task");
+        dialog.setHeaderText("Add new task");
+
+        //inputfield for å legge inn bruker:
+        TextField assignedUser = new TextField();
+        assignedUser.setPromptText("User");
+        System.out.println("Task was assigned to: " + assignedUser);
+
+        //inputfield for å legge inn oppgavenavn:
+        TextField taskName = new TextField();
+        taskName.setPromptText("Name of task");
+        System.out.println("Task name " + taskName + "was added");
+
+        //inputfieldsfor å legge inn poeng:
+        TextField pointsValue = new TextField();
+        pointsValue.setPromptText("0");
+        System.out.println("Points value " + pointsValue + "was added to task");
+
+        //inputfield for å legge inn dato:
+        TextField dueDay = new TextField();
+        dueDay.setPromptText("Due day");
+        System.out.println("Due day for task was set to: " + dueDay);
     }
 
 }
