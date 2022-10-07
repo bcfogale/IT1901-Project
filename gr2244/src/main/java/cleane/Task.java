2cfdde2996e95978c2498ee81872baae1bca4517
Sandra <sandrml@ntnu.no>
2022-10-07 10:42:31 +0200

    #34 Legge til knapp for å markere en oppgave som utført

--- gr2244/src/main/java/cleane/Task.java
+++ gr2244/src/main/java/cleane/Task.java
@@ -3,6 +3,8 @@ package cleane;
 import java.util.Arrays;
 import java.util.List;
 
+import javafx.scene.control.CheckBox;
+
 public class Task {
 
     final private List<String> days =Arrays.asList("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday");
@@ -10,8 +12,9 @@ public class Task {
     private User assignedUser;
     private String taskName;
     private int pointsValue;
-    private boolean completed = false;
+    private boolean completed;
     private String dueDay;
+    private CheckBox checkBox;
 
     
     public Task(User assignedUser, String taskName, int pointsValue, String dueDay) {
@@ -19,12 +22,14 @@ public class Task {
         assignedUser.addTask(this); // Adds this task to the assigned users task list
         this.taskName = taskName;
         this.pointsValue = pointsValue;
+        this.completed = false;
         if (days.contains(dueDay.toLowerCase())) {
             this.dueDay = dueDay;
         }
         else {
             throw new IllegalArgumentException("Choose a valid day.");
         }
+        checkBox.setSelected(false);
     }
 
     public void setTrue() {
@@ -43,12 +48,15 @@ public class Task {
         return pointsValue;
     }
 
-    public boolean isCompleted() {
-        return completed;
-    }
-
     public String getDueDay() {
         return dueDay;
+    }
+
+    public void completeTask() {
+        if (checkBox.isSelected()) {
+            this.setTrue();
+            assignedUser.addPoints(pointsValue);
+        }
     }
 
     @Override
@@ -56,13 +64,5 @@ public class Task {
         return "Taskname: " + taskName + "   " + "AssignedUser: " + assignedUser + "   " + "Points: " + pointsValue + "   " + "Dueday: " + dueDay;
     }
     
-
-    
-
-    
-
-    
-
-
 
 }
