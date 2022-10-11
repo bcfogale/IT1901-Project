644459a77f164162582271de109a3531ef6818a6
unknown <bcfogale@stud.ntnu.no>
2022-10-11 14:33:56 +0200

    Issue 29: Laget en test for sortList metoden i Leaderboard klassen. Medforfattet av @sandergs

--- gr2244/core/src/test/java/cleanE/LeaderboardTest.java
+++ gr2244/core/src/test/java/cleanE/LeaderboardTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import cleane.Leaderboard;
+import cleane.User;
 
 public class LeaderboardTest {
 
@@ -19,5 +20,37 @@ public class LeaderboardTest {
     @Test
     public void testConstructor(){
         assertEquals(0, l.getUsers().size());
+    }
+
+    @Test
+    public void testSortList() {
+        User u = new User("Sander");
+        User i = new User("Sandra");
+        User o = new User("Safura");
+
+        l.addUser(o);
+        l.addUser(u);
+        l.addUser(i);
+
+        u.addPoints(1);
+        i.addPoints(10);
+        o.addPoints(20);
+
+        assertEquals(o, l.getUsers().get(0));
+        assertEquals(u, l.getUsers().get(1));
+        assertEquals(i, l.getUsers().get(2));
+
+        l.sortList();
+
+        assertEquals(u, l.getUsers().get(0));
+        assertEquals(i, l.getUsers().get(1));
+        assertEquals(o, l.getUsers().get(2));
+
+
+    }
+
+    @Test
+    public void testAddUser() {
+        
     }
 }
