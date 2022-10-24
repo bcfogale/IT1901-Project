343fb45bff647a16869e0aa49ecb0a5bedf26310
Sander Garland Slinning <sandergs@stud.ntnu.no>
2022-10-24 10:17:55 +0200

    Issue #44 & #45: La til JavaDoc kommentarer i FileManagement-klassen

--- CleanE/core/src/main/java/json/FileManagement.java
+++ CleanE/core/src/main/java/json/FileManagement.java
@@ -13,6 +13,9 @@ public class FileManagement {
     private ObjectMapper mapper;
     private final File file = new File("../savestates/savefile.json");
 
+    /**
+     * Oppsett av mapper gjøres i konstruktøren.
+     */
     public FileManagement() {
         mapper = new ObjectMapper();
         CleanEModule mod = new CleanEModule();
@@ -20,10 +23,20 @@ public class FileManagement {
         mapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
     }
 
+    /**
+     * Metode som skriver til fil
+     * @param leaderboard -> Leaderboard-instansen du vil skrive til fil 
+     * @throws IOException
+     */
     public void writeToFile(Leaderboard leaderboard) throws IOException{
         mapper.writeValue(file, leaderboard);
     }
 
+    /**
+     * Metode som leser fra fil
+     * @return Leaderboard-instansen som leses inn og vises i programmet
+     * @throws IOException
+     */
     public Leaderboard readFromFile() throws IOException{
         return mapper.readerFor(Leaderboard.class).readValue(file);
         
