e3420149004a2c8b0e640dadfd5c3c066bb47575
unknown <bcfogale@stud.ntnu.no>
2022-11-16 16:13:45 +0100

    Issue #23: Laget kopi av eksisterende FXML fil som skal brukes av REST. Laget også en tilsvarende App-klasse som er bundet til den nye FXML filen.

---
+++ CleanE/fxui/src/main/java/ui/CleanERemoteApp.java
@@ -0,0 +1,21 @@
+package ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CleanERemoteApp extends Application{
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("CleanE");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("CleanERemoteApp.fxml"))));
+        primaryStage.show();
+    }
+}
