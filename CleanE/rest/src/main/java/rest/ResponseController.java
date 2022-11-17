dba05d5590e21163500d20b3bdb3ddb8c1993233
unknown <bcfogale@stud.ntnu.no>
2022-11-17 18:14:26 +0100

    Issue #23: Laget getters i ResponseController.java

--- CleanE/rest/src/main/java/rest/ResponseController.java
+++ CleanE/rest/src/main/java/rest/ResponseController.java
@@ -2,12 +2,17 @@
 
 package rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import core.Leaderboard;
+import core.User;
+
 
 
 // Annotation
@@ -18,23 +23,44 @@ public class ResponseController {
 	@Autowired
 	private CleanEService cleanEService;
 
-	private Leaderboard name() {
-		return new Leaderboard();
-	}
-
 	@GetMapping
 	public Leaderboard getLeaderboard() {
 	  return cleanEService.getLeaderboard();
 	}
-  
 
-	// 	// Creating object of ConsumeResponse class
-	// 	Leaderboard user = new cleanEService.getModel();
-	// 	model.addAttribute("response",
-	// 					data.get().getBody());
-	// 	model.addAttribute("headers",
-	// 					data.get().getHeaders());
+	@GetMapping(path = "/getUsers")
+	public List<User> getUsers() {
+		return getLeaderboard().getUsers();
+	}
 
-	// 	return "output";
-	// }
+	@GetMapping(path = "/getUser/{name}")
+	public User getUser(@PathVariable("name") String name) {
+		return getLeaderboard().getUser(name);
+	}
+
+	@GetMapping(path = "/getUser/{name}/getTaskByUUID/{uuid}/getTaskName")
+	public String getTaskName(@PathVariable("name") String name, @PathVariable("uuid") String uuid) {
+		return getLeaderboard().getUser(name).getTaskByUUID(uuid).getTaskName();
+	}
+	
+	@GetMapping(path = "/getUser/{name}/getTaskByUUID/{uuid}/getPointsValue")
+	public int getPointsValue(@PathVariable("name") String name, @PathVariable("uuid") String uuid) {
+		return getLeaderboard().getUser(name).getTaskByUUID(uuid).getPointsValue();
+	}
+
+	@GetMapping(path = "/getUser/{name}/getTaskByUUID/{uuid}/getDueDay")
+	public String getDueDay(@PathVariable("name") String name, @PathVariable("uuid") String uuid) {
+		return getLeaderboard().getUser(name).getTaskByUUID(uuid).getDueDay();
+	}
+
+	@GetMapping(path = "/getUser/{name}/getTaskByUUID/{uuid}/isCompleted")
+	public Boolean isCompleted(@PathVariable("name") String name, @PathVariable("uuid") String uuid) {
+		return getLeaderboard().getUser(name).getTaskByUUID(uuid).isCompleted();
+	}
+
+	@GetMapping(path = "/getUser/{name}/getTaskByUUID/{uuid}/getAssignedUser")
+	public User getAssignedUser(@PathVariable("name") String name, @PathVariable("uuid") String uuid) {
+		return getLeaderboard().getUser(name).getTaskByUUID(uuid).getAssignedUser();
+	}	
+	
 }
